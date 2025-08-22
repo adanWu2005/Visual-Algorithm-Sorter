d9ef3cbc4f0c020fa44276c77d40ef2a8cc69cbf
Adan Wu <162506142+adanWu2005@users.noreply.github.com>
2025-08-21 20:23:51 -0400

    Update Sorter.jsx

--- src/Sorter/Sorter.jsx
+++ src/Sorter/Sorter.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { getMergeSortAnimations, getBubbleSortAnimations, getQuickSortAnimations, getInsertionSortAnimations, getSelectionSortAnimations } from '../SortingAlgorithms/SortingAlgorithms.js';
 import './Sorter.css';
 
